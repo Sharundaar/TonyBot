15ab380f72483c19ada41f61013438b06baaca25
Sharundaar <rabiller.donatien@hotmail.fr>
2021-12-02 16:23:24 -0500

    first commit

---
+++ client.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import random
+import os
+
+description = '''Tony bot commands'''
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', description=description, intents=intents )
+treat_count = 0
+
+@bot.command()
+async def meow( ctx ):
+    emote = random.choice( ["<:tonyaah:860900789925183518>", "<:tonyhappy:860900538317406218>"])
+    await ctx.send( emote + ' meow ' + emote )
+
+@bot.command()
+async def moew( ctx ):
+    emote = random.choice( ["<:tonyaah:860900789925183518>", "<:tonyhappy:860900538317406218>"])
+    await ctx.send( emote + ' moew ' + emote )
+
+@bot.command()
+async def tonypic( ctx ):
+    pic = random.choice(open('pic-links.txt').read().splitlines())
+    await ctx.send(pic)
+
+@bot.command()
+async def treat( ctx ):
+    global treat_count
+    treat_count += 1
+    await ctx.send( f"<:tonyhappy:860900538317406218> <:tonyhappy:860900538317406218> <:tonyhappy:860900538317406218> meow onm onm onm onm meow <:tonyhappy:860900538317406218> <:tonyhappy:860900538317406218> <:tonyhappy:860900538317406218>\n\t\t\t\tJ'en ai mangé {treat_count} aujourd'hui.")
+
+bot.run('OTE2MDY0Mjk2OTE4NzE2NDQ2.YaktXA.WnUFd8VJnNP6Es5eWRFMU3HTafM')
